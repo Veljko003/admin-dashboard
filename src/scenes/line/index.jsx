e5e63e23f2fe83283c0cf2f0e7f98e4165209f9a
Veljko03 <105068570+Veljko003@users.noreply.github.com>
2023-02-08 20:20:37 +0100

    Add files via upload

---
+++ src/scenes/line/index.jsx
@@ -0,0 +1,19 @@
+// Imports
+import { Box } from "@mui/material";
+
+import Header from "../../components/Header";
+import LineChart from "../../components/LineChart";
+
+// Line function
+const Line = () => {
+  return (
+    <Box m="20px">
+      <Header title="Line Chart" subtitle="Simple Line Chart" />
+      <Box height="75vh">
+        <LineChart />
+      </Box>
+    </Box>
+  );
+};
+
+export default Line;
